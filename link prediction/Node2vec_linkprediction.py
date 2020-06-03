9d0f509b35bbf8ebbd7230ef67db6a7ee9bfc619
f1rstb100d <mmmjjjjjj1996@126.com>
2020-06-03 15:16:35 +0900

    Add files via upload

--- link prediction/Node2vec_linkprediction.py
+++ link prediction/Node2vec_linkprediction.py
@@ -154,33 +154,6 @@ class Node2Vec:
             self._embeddings[word] = self.w2v_model.wv[word]
         return self._embeddings
 
-    def __init__(self, graph, walk_length, num_walks):
-        self.graph = graph
-        self.w2v_model = None
-        self._embeddings = {}
-        self.walker = RandomWalker(graph)
-        self.sentences = self.walker.simulate_walks(num_walks=num_walks, walk_length=walk_length)
-
-    def train(self, embed_size=128, window_size=5, workers=3, iter=5, **kwargs):
-        kwargs["sentences"] = self.sentences
-        kwargs["min_count"] = kwargs.get("min_count", 0)
-        kwargs["size"] = embed_size
-        kwargs["sg"] = 1  # skip gram
-        kwargs["hs"] = 1  # deepwalk use Hierarchical Softmax
-        kwargs["workers"] = workers
-        kwargs["window"] = window_size
-        kwargs["iter"] = iter
-        print("Learning embedding vectors...")
-        model = Word2Vec(**kwargs)
-        print("Learning embedding vectors done!")
-        self.w2v_model = model
-        return model
-
-    def get_embeddings(self):
-        self._embeddings = {}
-        for word in self.graph.nodes():
-            self._embeddings[word] = self.w2v_model.wv[word]
-        return self._embeddings
 def split_train_test_graph(input_edgelist, seed, testing_ratio=0.01, weighted=False):
     if (weighted):
         G = nx.read_weighted_edgelist(input_edgelist)
