aa76d959dd63896b22abd091c52cceedd37f3ccb
f1rstb100d <mmmjjjjjj1996@126.com>
2020-07-08 19:55:45 +0900

    Add files via upload

--- stellar2.py
+++ stellar2.py
@@ -1,29 +1,24 @@
-# from stellargraph.datasets import IAEnronEmployees
+
 from sklearn.model_selection import train_test_split
 from stellargraph import StellarDiGraph
 import pandas as pd
 import numpy as np
 import random
-from stellargraph.data import TemporalRandomWalk
-# from stellargraph.data import BiasedRandomWalk
 from gensim.models import Word2Vec
-# from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
-# from sklearn.preprocessing import StandardScaler
 from stellargraph.random import random_state
 from stellargraph.core.graph import StellarGraph
 from stellargraph.core.schema import GraphSchema
 from scipy import stats
 from scipy.special import softmax
 
-# dataset = IAEnronEmployees()
-# full_graph, edges = dataset.load() #50572
+
 edges = pd.read_csv(
-            'ia-enron-employees.edges',
+            '../combinelink.txt',
             sep=" ",
             header=None,
-            names=["source", "target", "x", "time"],
+            names=["source", "target", "time"],
             usecols=["source", "target", "time"],
         )
 edges[["source", "target"]] = edges[["source", "target"]].astype(str)
@@ -32,106 +27,25 @@ nodes = pd.DataFrame(
                 pd.concat([edges["source"], edges["target"]], ignore_index=True)
             )
         )
+
 full_graph, edges = StellarDiGraph(nodes=nodes, edges=edges, edge_weight_column="time"), edges
 
 
-# subset of edges to split
-train_subset = 0.25
-test_subset = 0.25
-
-# number of edges to be kept in the graph
-num_edges_graph = int(len(edges) * (1 - train_subset)) # 37929
-
-# keep older edges in graph, and predict more recent edges
-edges_graph = edges[:num_edges_graph] # 37929
-edges_other = edges[num_edges_graph:] # 12643
-
-# split recent edges further to train and test sets
-edges_train, edges_test = train_test_split(edges_other, test_size=test_subset)
-
-print(
-    f"Number of edges in graph: {len(edges_graph)}\n"
-    f"Number of edges in training set: {len(edges_train)}\n" # 9482
-    f"Number of edges in test set: {len(edges_test)}" # 3161
-)
-
 graph = StellarDiGraph(
     nodes=pd.DataFrame(index=full_graph.nodes()),
-    edges=edges_graph,
+    edges=edges,
     edge_weight_column="time",
 )
 
-def positive_and_negative_links1(g, edges1, edges2):
-    pos1 = list(edges1[["source", "target"]].itertuples(index=False))
-    pos_all = pd.concat([edges1, edges2], ignore_index=True)
-    # pos2 = list(edges2[["source", "target"]].itertuples(index=False))
-    neg = sample_negative_examples1(g, pos_all)
-    return pos1, neg
-
-
-def sample_negative_examples1(g, positive_examples):
-    positive_set = set(positive_examples)
-
-    def valid_neg_edge(src, tgt):
-        return (
-            # no self-loops
-            src != tgt
-            and
-            # neither direction of the edge should be a positive one
-            (src, tgt) not in positive_set
-            # and (tgt, src) not in positive_set
-        )
-
-    possible_neg_edges = [
-        (src, tgt) for src in g.nodes() for tgt in g.nodes() if valid_neg_edge(src, tgt)
-    ]
-
-    return random.sample(possible_neg_edges, k=len(edges_train))
-
-def positive_and_negative_links2(g, edges1, edges2):
-    # pos1 = list(edges1[["source", "target"]].itertuples(index=False))
-    pos_all = pd.concat([edges1, edges2], ignore_index=True)
-    pos2 = list(edges2[["source", "target"]].itertuples(index=False))
-    neg = sample_negative_examples2(g, pos_all)
-    return pos2, neg
-
-
-def sample_negative_examples2(g, positive_examples):
-    positive_set = set(positive_examples)
-
-    def valid_neg_edge(src, tgt):
-        return (
-            # no self-loops
-            src != tgt
-            and
-            # neither direction of the edge should be a positive one
-            (src, tgt) not in positive_set
-            # and (tgt, src) not in positive_set
-        )
-
-    possible_neg_edges = [
-        (src, tgt) for src in g.nodes() for tgt in g.nodes() if valid_neg_edge(src, tgt)
-    ]
-
-    return random.sample(possible_neg_edges, k=len(edges_test))
-
-
-pos, neg = positive_and_negative_links1(graph, edges_train, edges_test)
-pos_test, neg_test = positive_and_negative_links2(graph, edges_train, edges_test)
-
-print(
-    f"{graph.info()}\n"
-    f"Training examples: {len(pos)} positive links, {len(neg)} negative links\n" # 9482
-    f"Test examples: {len(pos_test)} positive links, {len(neg_test)} negative links" # 3161
-)
 
 
 num_walks_per_node = 10
 walk_length = 80
 context_window_size = 10
 
-num_cw = len(graph.nodes()) * num_walks_per_node * (walk_length - context_window_size + 1) # 107210
+num_cw = len(graph.nodes()) * num_walks_per_node * (walk_length - context_window_size + 1) # 633*10*71 = 449430
 
+print(num_cw)
 
 
 def naive_weighted_choices(rs, weights):
@@ -193,6 +107,7 @@ class TemporalRandomWalk():
 
         # loop runs until we have enough context windows in total
         while num_cw_curr < self.num_cw:
+            print(num_cw_curr)
             first_edge_index = self._sample(len(times), edge_biases, np_rs)
             src = sources[first_edge_index]
             dst = targets[first_edge_index]
@@ -246,11 +161,10 @@ class TemporalRandomWalk():
     def _step(self, node, time, bias_type, np_rs):
         """
         Perform 1 temporal step from a node. Returns None if a dead-end is reached.
-
         """
         neighbours, times = self.graph.neighbor_arrays(node, include_edge_weight=True)
-        neighbours = neighbours[times > time]
-        times = times[times > time]
+        neighbours = neighbours[times >= time]
+        times = times[times >= time]
 
         if len(neighbours) > 0:
             biases = self._temporal_biases(times, time, bias_type, is_forward=True)
@@ -296,6 +210,103 @@ temporal_model = Word2Vec(
     iter=1,
 )
 
+
+
+edges_0620 = pd.read_csv(
+            '../relink0620.txt',
+            sep=" ",
+            header=None,
+            names=["source", "target",],
+            usecols=["source", "target"],
+        )
+edges_0620[["source", "target"]] = edges_0620[["source", "target"]].astype(str)
+full_graph_0620, edges_0620 = StellarDiGraph(nodes=nodes, edges=edges_0620), edges_0620
+
+
+edges_train_pos, edges_test_pos = train_test_split(edges_0620, test_size=0.2)
+
+
+
+
+def positive_and_negative_links1(g, edges1, edges2):
+    pos1 = list(edges1[["source", "target"]].itertuples(index=False))
+    pos_all = pd.concat([edges1, edges2], ignore_index=True)
+    # pos2 = list(edges2[["source", "target"]].itertuples(index=False))
+    neg = sample_negative_examples1(g, pos_all)
+    return pos1, neg
+
+
+def sample_negative_examples1(g, positive_examples):
+    positive_set = set(positive_examples)
+
+    def valid_neg_edge(src, tgt):
+        return (
+            # no self-loops
+            src != tgt
+            and
+            # neither direction of the edge should be a positive one
+            (src, tgt) not in positive_set
+            # and (tgt, src) not in positive_set
+        )
+
+    possible_neg_edges = [
+        (src, tgt) for src in g.nodes() for tgt in g.nodes() if valid_neg_edge(src, tgt)
+    ]
+
+    return random.sample(possible_neg_edges, k=len(edges_train_pos))
+
+
+
+def positive_and_negative_links2(g, edges1, edges2):
+    # pos1 = list(edges1[["source", "target"]].itertuples(index=False))
+    pos_all = pd.concat([edges1, edges2], ignore_index=True)
+    pos2 = list(edges2[["source", "target"]].itertuples(index=False))
+    neg = sample_negative_examples2(g, pos_all)
+    return pos2, neg
+
+
+def sample_negative_examples2(g, positive_examples):
+    positive_set = set(positive_examples)
+
+    def valid_neg_edge(src, tgt):
+        return (
+            # no self-loops
+            src != tgt
+            and
+            # neither direction of the edge should be a positive one
+            (src, tgt) not in positive_set
+            # and (tgt, src) not in positive_set
+        )
+
+    possible_neg_edges = [
+        (src, tgt) for src in g.nodes() for tgt in g.nodes() if valid_neg_edge(src, tgt)
+    ]
+
+    return random.sample(possible_neg_edges, k=len(edges_test_pos))
+
+
+
+pos_train, neg_train = positive_and_negative_links1(full_graph_0620, edges_train_pos, edges_test_pos)
+pos_test, neg_test = positive_and_negative_links2(full_graph_0620, edges_train_pos, edges_test_pos)
+
+print(
+    f"{graph.info()}\n"
+    f"Training examples: {len(pos_train)} positive links, {len(neg_train)} negative links\n" # 9482
+    f"Test examples: {len(pos_test)} positive links, {len(neg_test)} negative links" # 3161
+)
+
+
+
+
+def labelled_links(positive_examples, negative_examples):
+    return (positive_examples + negative_examples, np.repeat([1, 0], [len(positive_examples), len(negative_examples)]))
+
+
+
+link_examples_train, link_labels_train = labelled_links(pos_train, neg_train)
+link_examples_test, link_labels_test = labelled_links(pos_test, neg_test)
+
+
 unseen_node_embedding = np.zeros(embedding_size)
 
 
@@ -306,9 +317,7 @@ def temporal_embedding(u):
         return unseen_node_embedding
 
 
-
 def operator_l2(u, v):
-    # return (u - v) ** 2
     return np.concatenate((u, v), axis=0)
 
 binary_operator = operator_l2
@@ -324,19 +333,18 @@ def link_examples_to_features(link_examples, transform_node):
 
 
 
+temporal_clf = LogisticRegression(solver='lbfgs', max_iter=5000)
+temporal_link_features_train = link_examples_to_features(link_examples_train, temporal_embedding)
+temporal_link_features_test = link_examples_to_features(link_examples_test, temporal_embedding)
+
+temporal_clf.fit(temporal_link_features_train, link_labels_train)
+
+
 def evaluate_roc_auc(clf, link_features, link_labels):
     predicted = clf.predict_proba(link_features)
     positive_column = list(clf.classes_).index(1)
     return roc_auc_score(link_labels, predicted[:, positive_column])
 
-def labelled_links(positive_examples, negative_examples):
-    return (positive_examples + negative_examples, np.repeat([1, 0], [len(positive_examples), len(negative_examples)]))
 
-link_examples, link_labels = labelled_links(pos, neg)
-link_examples_test, link_labels_test = labelled_links(pos_test, neg_test)
-temporal_clf = LogisticRegression(solver='lbfgs', max_iter=5000)
-temporal_link_features = link_examples_to_features(link_examples, temporal_embedding)
-temporal_link_features_test = link_examples_to_features(link_examples_test, temporal_embedding)
-temporal_clf.fit(temporal_link_features, link_labels)
 temporal_score = evaluate_roc_auc(temporal_clf, temporal_link_features_test, link_labels_test)
 print(f"Score (ROC AUC): {temporal_score:.2f}")
